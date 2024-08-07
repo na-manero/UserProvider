06fd2c75313b3f011feb76b610f7d1928ab2666f
Chris <christofferjcm@gmail.com>
2024-08-07 09:23:05 +0200

    update db

---
+++ UserProvider.Data/Migrations/20240807070003_UpdateProfile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserProvider.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "UserProfiles");
+
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "UserProfiles",
+                newName: "Location");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Location",
+                table: "UserProfiles",
+                newName: "PhoneNumber");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
